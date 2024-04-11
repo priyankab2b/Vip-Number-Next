a2df0f670cc1c95069bb7c494bdb3b9daad0009c
techbase2brand <techbase2brand@gmail.com>
2024-04-11 18:51:45 +0530

    files deleted 110424

--- src/app/Shared/City/CityHeading/CityHeading.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./CityHeading.css";
-
-const CityHeading = ({ title }) => {
-  return <h2 className="CityHeading-heading-os">{title}</h2>;
-};
-
-export default CityHeading;
